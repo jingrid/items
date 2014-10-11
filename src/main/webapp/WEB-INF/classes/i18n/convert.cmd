25de39006c3fcc4cd4a2ba3b3fe73aac8215e3d5
henryleu <henryleu@126.com>
2014-10-11 19:01:18 +0800

    initial

--- src/main/webapp/WEB-INF/classes/i18n/convert.cmd
+++
@@ -1,3 +0,0 @@
-native2ascii -encoding gb2312 AppResource_zh_CN.txt AppResource_zh_CN.properties
-copy AppResource_en.txt AppResource_en.properties
-copy AppResource_zh_CN.properties AppResource.properties
